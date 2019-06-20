450630be593506d10e501507ef4be35d28cef329
Marcos Lilljedahl <marcosnils@gmail.com>
2019-06-20 04:38:46 -0300

    Add docker folder and new build action

---
+++ docker/cli/entrypoint.sh
@@ -0,0 +1,45 @@
+#!/bin/sh -l
+
+
+set -exo pipefail
+
+if [ "$1" = "build" ]; then
+
+    eval DIR=\${$#}
+
+    TAG=$(echo "$GITHUB_REF" | cut -f3 -d'/')
+
+    # By default use previous commit as REF
+    PREVIOUS_REF=$(git rev-parse HEAD^1)
+
+    # IF there is a tag, use the tag
+    if [ -z "$TAG" ]; then
+        # tries to get the previous tag. If first one, return same
+        PREVIOUS_REF="$(git describe --tags --abbrev=0 "tags/$TAG^" || $TAG)"
+
+    fi
+
+
+    should_build() {
+            # --quiet will exit 1 if there are differences and 0 if none.
+
+            # should deploy if lambda was changed
+            git diff --quiet HEAD "$PREVIOUS_REF" -- "$DIR" || return 0
+
+            # should deploy if workflows were changed
+            git diff --quiet HEAD "$PREVIOUS_REF" -- .github/main.workflow || return 0
+
+            # otherwise do not deploy.
+            return 1
+    }
+
+    # Exit successfully if nothing to build 
+    should_build || {
+        echo "$DIR hasn't changed since last commit or release $PREVIOUS_REF, no need to build"
+        exit 0
+    }
+
+fi
+
+
+sh -c "docker $*"
